83421aa8829bd58eeb691a1129acb28cd767422b
Armando233-abc <armandovigliotti5@gmail.com>
2023-09-15 17:45:57 +0200

    feat: new title component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './Card.module.css'
+import Title from '../Title'
 
 const Card = () => {
       return (
             <div className={styles.body}>
                   <div className={styles.card__container}>
-                        <h1>ciao</h1>
+                        <Title></Title>
                   </div>
             </div>
       )
